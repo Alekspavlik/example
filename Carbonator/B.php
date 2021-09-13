62a5c8b740f8ca03c4c8fd60fc5b1dc808ef1dec
oleksandrmorozov <morozovoleksandr86@gmail.com>
2021-09-13 22:20:23 +0300

    Проработать работу авто загрузчика, показанную на уроке.

---
+++ Carbonator/B.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Tuktuk;
+
+class B
+{
+
+}
--- public/index.php
+++ public/index.php
@@ -2,23 +2,6 @@
 
 require_once "../vendor/autoload.php";
 
-use \App\ {
-    Currency,
-    Money
-};
 
-$usd = new Currency("USD");
-$eur = new Currency("EUR");
-
-$money1 = new Money(50, $usd);
-$money2 = new Money(500, $usd);
-
-if ($money1->equals($money2)) {
-    echo "All right";
-}
-
-$money3 = $money1->add($money2);
-
-echo "<pre>";
-var_dump($money3);
-echo "<pre>";
+new \App\A();
+new \Tuktuk\B();
